f9bc47ef96d39648249a68242a6a894eabc44f85
Ratschiller <gabriel.ratschiller@int.rubicon-it.com>
2020-08-07 14:07:55 +0200

    Change MenuCommand to OleMenuCommand

--- LicenseHeaderManager/CommandsAsync/ProjectItemMenu/RemoveHeaderFromProjectItemCommandAsync.cs
+++ LicenseHeaderManager/CommandsAsync/ProjectItemMenu/RemoveHeaderFromProjectItemCommandAsync.cs
@@ -39,7 +39,7 @@ namespace LicenseHeaderManager.CommandsAsync.ProjectItemMenu
       commandService = commandService ?? throw new ArgumentNullException (nameof (commandService));
 
       var menuCommandID = new CommandID (CommandSet, CommandId);
-      var menuItem = new MenuCommand (this.Execute, menuCommandID);
+      var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
       commandService.AddCommand (menuItem);
     }
 
