e5e48fdeeda23628e2e52eb9f843ff914292ae9e
Gabriel Ratschiller <gabriel.ratschiller@rubicon.eu>
2020-09-07 11:19:19 +0200

    Prevent to insert header into new files, when downloaded and opened by programs (e.g. DependDB)

--- LicenseHeaderManager/Utils/Extensions.cs
+++ LicenseHeaderManager/Utils/Extensions.cs
@@ -32,7 +32,7 @@ namespace LicenseHeaderManager.Utils
   /// </summary>
   internal static class Extensions
   {
-    private static readonly ILog s_log = LogManager.GetLogger (MethodBase.GetCurrentMethod().DeclaringType);
+    private static readonly ILog s_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
     /// <summary>
     ///   Sets the <see cref="LicenseHeaderPathInput.IgnoreNonCommentText" /> property to <see langword="true" /> for all
@@ -47,7 +47,7 @@ namespace LicenseHeaderManager.Utils
     ///   remove operations. In that case,
     ///   no confirmations regarding non-comment text are needed.
     /// </remarks>
-    public static void IgnoreNonCommentText (this IEnumerable<LicenseHeaderContentInput> inputs)
+    public static void IgnoreNonCommentText(this IEnumerable<LicenseHeaderContentInput> inputs)
     {
       foreach (var licenseHeaderInput in inputs)
         licenseHeaderInput.IgnoreNonCommentText = true;
@@ -57,9 +57,9 @@ namespace LicenseHeaderManager.Utils
     ///   Replaces occurrences of "\n" in a string by new line characters.
     /// </summary>
     /// <returns>A <see cref="string" /> where all occurrences of "\n" have been replaced by new line characters.</returns>
-    public static string ReplaceNewLines (this string input)
+    public static string ReplaceNewLines(this string input)
     {
-      return input.Replace (@"\n", "\n");
+      return input.Replace(@"\n", "\n");
     }
 
     /// <summary>
@@ -67,7 +67,7 @@ namespace LicenseHeaderManager.Utils
     /// </summary>
     /// <param name="item">Specifies the project item of which the additional properties should be extracted.</param>
     /// <returns></returns>
-    public static IEnumerable<AdditionalProperty> GetAdditionalProperties (this ProjectItem item)
+    public static IEnumerable<AdditionalProperty> GetAdditionalProperties(this ProjectItem item)
     {
       ThreadHelper.ThrowIfNotOnUIThread();
 
@@ -110,9 +110,9 @@ namespace LicenseHeaderManager.Utils
              };
     }
 
-    private static AdditionalProperty CreateAdditionalProperty (string token, Func<bool> canCreateValue, Func<string> createValue)
+    private static AdditionalProperty CreateAdditionalProperty(string token, Func<bool> canCreateValue, Func<string> createValue)
     {
-      return new AdditionalProperty (token, canCreateValue() ? createValue() : token);
+      return new AdditionalProperty(token, canCreateValue() ? createValue() : token);
     }
 
     /// <summary>
@@ -122,30 +122,30 @@ namespace LicenseHeaderManager.Utils
     /// <param name="extension">Specifies the extension of the language whose content should be added to the project item.</param>
     /// <param name="calledByUser">Specifies whether this method was called explicitly by the user or by the program.</param>
     /// <returns></returns>
-    public static async Task AddLicenseHeaderToItemAsync (
+    public static async Task AddLicenseHeaderToItemAsync(
         this ProjectItem item,
         ILicenseHeaderExtension extension,
         bool calledByUser)
     {
-      if (item == null || ProjectItemInspection.IsLicenseHeader (item))
+      if (item == null || ProjectItemInspection.IsLicenseHeader(item))
         return;
 
       await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-      var headers = LicenseHeaderFinder.GetHeaderDefinitionForItem (item);
+      var headers = LicenseHeaderFinder.GetHeaderDefinitionForItem(item);
       if (headers != null)
       {
-        var content = item.GetContent (out var wasAlreadyOpen, extension);
+        var content = item.GetContent(out var wasAlreadyOpen, extension);
         if (content == null)
           return;
 
-        var result = await extension.LicenseHeaderReplacer.RemoveOrReplaceHeader (
-            new LicenseHeaderContentInput (content, item.FileNames[1], headers, item.GetAdditionalProperties()));
-        await CoreHelpers.HandleResultAsync (result, extension, wasAlreadyOpen, calledByUser);
+        var result = await extension.LicenseHeaderReplacer.RemoveOrReplaceHeader(
+            new LicenseHeaderContentInput(content, item.FileNames[1], headers, item.GetAdditionalProperties()));
+        await CoreHelpers.HandleResultAsync(result, extension, wasAlreadyOpen, calledByUser);
         return;
       }
 
-      if (calledByUser && LicenseHeader.ShowQuestionForAddingLicenseHeaderFile (item.ContainingProject, extension.DefaultLicenseHeaderPageModel))
-        await AddLicenseHeaderToItemAsync (item, extension, true);
+      if (calledByUser && LicenseHeader.ShowQuestionForAddingLicenseHeaderFile(item.ContainingProject, extension.DefaultLicenseHeaderPageModel))
+        await AddLicenseHeaderToItemAsync(item, extension, true);
     }
 
     /// <summary>
@@ -156,32 +156,39 @@ namespace LicenseHeaderManager.Utils
     /// <param name="wasAlreadyOpen">Specifies whether the project item was already open before this method was called.</param>
     /// <param name="extension">Specifies the extension of the language whose content should be returned.</param>
     /// <returns></returns>
-    public static string GetContent (this ProjectItem item, out bool wasAlreadyOpen, ILicenseHeaderExtension extension)
+    public static string GetContent(this ProjectItem item, out bool wasAlreadyOpen, ILicenseHeaderExtension extension)
     {
       ThreadHelper.ThrowIfNotOnUIThread();
 
       wasAlreadyOpen = item.IsOpen();
 
-      if (!wasAlreadyOpen && !CoreHelpers.TryOpenDocument (item, extension))
+      if (!wasAlreadyOpen && !CoreHelpers.TryOpenDocument(item, extension))
         return null;
 
       // Referring to the comment in the TryOpenDocument method: files with unknown extensions that were precautiously not opened are still fed as input to the
       // Core - with empty content, though, as the Core will return ReplacerErrorType.LanguageNotFound anyway (i. e. content is not relevant, only the extension)
       // (returning false from TryOpenDocument or null from this method would mean skipping the file entirely, which we do not want since we want to be able to
       // react to a ReplacerErrorType.LanguageNotFound)
-      var languageForExtension = extension.LicenseHeaderReplacer.GetLanguageFromExtension (Path.GetExtension (item.FileNames[1]));
+      var languageForExtension = extension.LicenseHeaderReplacer.GetLanguageFromExtension(Path.GetExtension(item.FileNames[1]));
       if (languageForExtension == null)
         return "";
 
-      if (!(item.Document.Object ("TextDocument") is TextDocument textDocument))
+      try
+      {
+        if (!(item.Document?.Object("TextDocument") is TextDocument textDocument))
+          return null;
+
+        var wasSaved = item.Document.Saved;
+        var content = textDocument.CreateEditPoint(textDocument.StartPoint).GetText(textDocument.EndPoint);
+
+        CoreHelpers.SaveAndCloseIfNecessary(item, wasAlreadyOpen, wasSaved);
+
+        return content;
+      }
+      catch (Exception)
+      {
         return null;
-
-      var wasSaved = item.Document.Saved;
-      var content = textDocument.CreateEditPoint (textDocument.StartPoint).GetText (textDocument.EndPoint);
-
-      CoreHelpers.SaveAndCloseIfNecessary (item, wasAlreadyOpen, wasSaved);
-
-      return content;
+      }
     }
 
     /// <summary>
@@ -189,7 +196,7 @@ namespace LicenseHeaderManager.Utils
     /// </summary>
     /// <param name="item">Specifies the project item to be checked if it is open.</param>
     /// <returns></returns>
-    private static bool IsOpen (this ProjectItem item)
+    private static bool IsOpen(this ProjectItem item)
     {
       ThreadHelper.ThrowIfNotOnUIThread();
 
@@ -199,34 +206,34 @@ namespace LicenseHeaderManager.Utils
       }
       catch (COMException ex)
       {
-        s_log.Error ("Could not determine if project item is open. Assuming it is closed.", ex);
+        s_log.Error("Could not determine if project item is open. Assuming it is closed.", ex);
         return false;
       }
     }
 
-    public static void FireAndForget (this Task task)
+    public static void FireAndForget(this Task task)
     {
       // note: this code is inspired by a tweet from Ben Adams: https://twitter.com/ben_a_adams/status/1045060828700037125
       // Only care about tasks that may fault (not completed) or are faulted,
       // so fast-path for SuccessfullyCompleted and Canceled tasks.
       if (!task.IsCompleted || task.IsFaulted)
-          // use "_" (Discard operation) to remove the warning IDE0058: Because this call is not awaited, execution of the current method continues before the call is completed
-          // https://docs.microsoft.com/en-us/dotnet/csharp/discards#a-standalone-discard
-        _ = ForgetAwaited (task);
+        // use "_" (Discard operation) to remove the warning IDE0058: Because this call is not awaited, execution of the current method continues before the call is completed
+        // https://docs.microsoft.com/en-us/dotnet/csharp/discards#a-standalone-discard
+        _ = ForgetAwaited(task);
 
       // Allocate the async/await state machine only when needed for performance reason.
       // More info about the state machine: https://blogs.msdn.microsoft.com/seteplia/2017/11/30/dissecting-the-async-methods-in-c/
-      static async Task ForgetAwaited (Task task)
+      static async Task ForgetAwaited(Task task)
       {
         try
         {
           // No need to resume on the original SynchronizationContext, so use ConfigureAwait(false)
-          await task.ConfigureAwait (false);
+          await task.ConfigureAwait(false);
         }
         catch (Exception ex)
         {
-          MessageBoxHelper.ShowError ($"Asynchronous Task failed: {ex.Message}\nSee output pane or log file for more details.", Resources.TaskFailed);
-          s_log.Error ("Asynchronous Task failed", ex);
+          MessageBoxHelper.ShowError($"Asynchronous Task failed: {ex.Message}\nSee output pane or log file for more details.", Resources.TaskFailed);
+          s_log.Error("Asynchronous Task failed", ex);
         }
       }
     }
